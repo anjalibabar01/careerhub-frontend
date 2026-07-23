342122c0452337fe78f0b0933a93ba2bfb0c1b0d
anjalibabar01 <anjalibabar9192@gmail.com>
2026-07-23 22:56:55 +0530

    connect frontend to deployed backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8081",
+  baseURL: "https://careerhub-backend-uk0z.onrender.com",
 });
 
 api.interceptors.request.use(
